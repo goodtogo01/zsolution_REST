3b006f9e7878e5bfe8cac5ca5bafb771c3b1c4fd
kz1985 <khosruzzaman.ny@gmail.com>
2024-05-26 21:35:24 -0400

    new class added

--- src/test/java/com/Pages/Students.java
+++ src/test/java/com/Pages/Students.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.http.HttpStatus;
+import org.apache.http.protocol.HTTP;
 import org.hamcrest.core.Is;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -19,14 +20,13 @@ public class Students {
 	String groupName = "Group:4";
 	static Response response;
 	String endPoints = "http://localhost:3000/Students/";
-	String ep_WithId = "http://localhost:3000/Students?id=123";
+	String ep_WithId = "http://localhost:3000/Students?id=325";
 
 	@Test
 	public void getAllStudents() {
-		response = RestAssured.given().contentType(ContentType.JSON).accept("application/json").when().get(ep_WithId)
-				.then().body("Students[2].firstName", Is.is("Kola"))
-				
-				.log().all().extract().response();
+		response = RestAssured.given().contentType(ContentType.JSON).accept("application/json")
+				.when().get(ep_WithId)
+				.then().log().all().extract().response();
 		int status_code = response.statusCode();
 		System.out.println("Status code found : " + status_code);
 		Assert.assertEquals(status_code, 200);
@@ -38,23 +38,17 @@ public class Students {
 
 		Map<String, Object> data = new HashMap<>();
 		data.put("departmentsGroup", "Group:4");
-		data.put("id", "5500");
+		data.put("id", 325);
 		data.put("firstName", "Kola");
 		data.put("lastName", "Mia");
 		data.put("email", "KolaMia@zsolution.com");
-		
-		int newStudent = RestAssured.given()
-				.contentType(ContentType.JSON)
-				.accept("application/json")
-				.when().post(endPoints)
-				.then()
-				.assertThat()
-				.statusCode(HttpStatus.SC_CREATED)
-				.extract()
-				.path("id");
-	
-		
-		
+
+		response = RestAssured.given().contentType(ContentType.JSON).accept("application/json").when().body(data)
+				.post(endPoints).then().assertThat().statusCode(201).log().all().extract().response();
+
+		int status_code = response.statusCode();
+		System.out.println("Status code found : " + status_code);
+		Assert.assertEquals(status_code, 201);
 
 	}
 
---
+++ src/test/java/com/Pages/User_With_Token.java
@@ -0,0 +1,40 @@
+package com.Pages;
+
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class User_With_Token {
+    
+public String token;
+    @Test(priority = 1)
+    public void validateToken(){
+        Map<String, String> data = new HashMap<>();
+        data.put("email","john@mail.com");
+        data.put("password","changeme");
+
+        Response response = given().header("Content-Type", "application/json")
+                .body(data).when().post("https://api.escuelajs.co/api/v1/auth/login");
+
+        Assert.assertEquals(response.statusCode(), 200);
+        //Assert.assertEquals(response.body().jsonPath().getString("message"), "User Varified successfully");
+        response.prettyPrint();
+        token = response.body().jsonPath().getString("access_token");
+    }
+
+    @Test(priority = 2)
+    public void getUserProfile(){
+        Response response = given().header("Content-Type", "application/json")
+                .header("Authorization", "Bearer"+token)
+                .when().get("https://api.escuelajs.co/api/v1/auth/profile/")
+                .then().log().all().extract().response();
+        Assert.assertEquals(response.statusCode(), 200);
+      //  Assert.assertEquals(response.body().jsonPath().getString("message"), "Data received successfully");
+        response.prettyPrint();
+    }
+}
